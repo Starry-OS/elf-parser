e1a6e94edfa3d830c747198bce8ef31219f827e4
Youjie Zheng <zyj578335934@gmail.com>
2024-09-16 16:41:07 +0845

    Update README

--- src/auxv.rs
+++ src/auxv.rs
@@ -1,10 +1,9 @@
 //! Some constant in the elf file
 extern crate alloc;
-use alloc::{collections::BTreeMap, string::String, vec::Vec};
+use alloc::collections::BTreeMap;
 use log::info;
-use memory_addr::{VirtAddr, PAGE_SIZE_4K};
+use memory_addr::PAGE_SIZE_4K;
 
-use crate::user_stack::init_stack;
 const AT_PHDR: u8 = 3;
 const AT_PHENT: u8 = 4;
 const AT_PHNUM: u8 = 5;
@@ -56,43 +55,4 @@ pub fn get_auxv_vector(
     map.insert(AT_RANDOM, 0);
     map.insert(AT_PAGESZ, PAGE_SIZE_4K);
     map
-}
-/// To get the app stack and the information on the stack from the ELF file
-///
-/// # Arguments
-///
-/// * `args` - The arguments of the app
-/// * `envs` - The environment variables of the app
-/// * `auxv` - The auxv vector of the app
-/// * `stack_top` - The top address of the stack
-/// * `stack_size` - The size of the stack.
-///
-/// # Return
-///
-/// `(stack_content, real_stack_bottom)`
-///
-/// * `stack_content`: the stack data from the low address to the high address, which will be used to map in the memory
-///
-/// * `real_stack_bottom`: The initial stack bottom is `stack_top + stack_size`.After push arguments into the stack, it will return the real stack bottom
-///
-/// The return data will be divided into two parts.
-/// * The first part is the free stack content, which is all 0.
-/// * The second part is the content carried by the user stack when it is initialized, such as args, auxv, etc.
-///
-/// The detailed format is described in <https://articles.manugarg.com/aboutelfauxiliaryvectors.html>
-pub fn get_app_stack_region(
-    args: Vec<String>,
-    envs: &[String],
-    auxv: BTreeMap<u8, usize>,
-    stack_top: VirtAddr,
-    stack_size: usize,
-) -> (Vec<u8>, usize) {
-    let ustack_top = stack_top;
-    let ustack_bottom = ustack_top + stack_size;
-    // The stack variable is actually the information carried by the stack
-    let stack = init_stack(args, envs, auxv, ustack_bottom.into());
-    let ustack_bottom = stack.get_sp();
-    let mut data = [0_u8].repeat(stack_size - stack.get_len());
-    data.extend(stack.get_data_front_ref());
-    (data, ustack_bottom)
 }
